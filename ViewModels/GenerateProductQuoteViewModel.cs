1700316a38ec9636a1def8e5fac20c3660a6bb2d
Younous B. Houssen <jonashoussen@gmail.com>
2025-10-17 20:12:50 +0200

    Add Technician with Dashboard, Repair Jobs, and Quote Management

---
+++ ViewModels/GenerateProductQuoteViewModel.cs
@@ -0,0 +1,7 @@
+namespace SupertronicsRepairSystem.ViewModels.Technician {
+    public class GenerateProductQuoteViewModel {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
