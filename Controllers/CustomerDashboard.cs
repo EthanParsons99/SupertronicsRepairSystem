04ab98903fa76d00b3c5713288f5cff409f77b45
Joshua Thomas <105593188+Joshcybr@users.noreply.github.com>
2025-10-19 23:03:04 +0200

    cool, basic customer dashboard

---
+++ Controllers/CustomerDashboard.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SupertronicsRepairSystem.Models;
+
+
+namespace SupertronicsRepairSystem.Controllers
+{
+    public class CustomerDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            // Simulate fetching data from a database or service
+            var productsOnSale = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Acer Aspire 3",
+                    Description = "15.6-inch FHD Laptop with Intel Core i7",
+                    ImageUrl = "/images/acer-laptop.png", // Assuming image is in wwwroot/images
+                    Price = 9999m,
+                    WasPrice = 10999m,
+                    DiscountPercentage = 9
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "JBL Live 770NC",
+                    Description = "Noise Cancelling Wireless Headphones",
+                    ImageUrl = "/images/jbl-headphones.png", // Assuming image is in wwwroot/images
+                    Price = 1498m,
+                    WasPrice = 2498m,
+                    DiscountPercentage = 40
+                }
+            };
+
+            // Pass the list of products to the view
+            return View(productsOnSale);
+        }
+    }
+}
