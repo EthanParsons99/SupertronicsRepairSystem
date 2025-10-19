73408edd49e7f0c570f809ac647a7c831621d382
Joshua Thomas <105593188+Joshcybr@users.noreply.github.com>
2025-10-19 23:03:04 +0200

    cool, basic customer dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,20 +4,15 @@ using SupertronicsRepairSystem.Models;
 
 namespace SupertronicsRepairSystem.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+   
+        public class HomeController : Controller
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+            public IActionResult Index()
+            {
+                
+                return View();
+            }
+     
         public IActionResult Privacy()
         {
             return View();
