8777b32429d7ee568a6efa1d8751892f0a4c4faa
PC <PC@CALWYN-PC>
2025-10-25 07:24:19 +0200

    added final fix for choosing quote type

---
+++ ViewModels/SelectJobForQuoteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupertronicsRepairSystem.ViewModels.Technician
+{
+    public class SelectJobForQuoteViewModel
+    {
+        [Required(ErrorMessage = "You must select a repair job.")]
+        [Display(Name = "Select an Open Repair Job")]
+        public string SelectedJobId { get; set; }
+
+        public List<SelectListItem> OpenJobs { get; set; }
+    }
+}
