c4853a68914a7a232297dad0543719419d3cb7db
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-10-25 18:53:32 +0200

    added product services and viewmodel

---
+++ Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using SupertronicsRepairSystem.Models;
+using SupertronicsRepairSystem.ViewModels;
+using System.Threading.Tasks;
+
+namespace SupertronicsRepairSystem.Services
+{
+    public interface IProductService
+    {
+       Task<bool> AddProductAsync(AddProductViewModel model);
+    }
+}
