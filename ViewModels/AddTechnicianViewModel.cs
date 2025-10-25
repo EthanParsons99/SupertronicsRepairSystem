2b458b7e363468cfd1fba5346d967a9fe2319ba5
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-10-25 18:26:17 +0200

    Logic added for owner to add new technicians

---
+++ ViewModels/AddTechnicianViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SupertronicsRepairSystem.ViewModels
+{
+    public class AddTechnicianViewModel
+    {
+        [Required(ErrorMessage = "Technician name is required.")]
+        [StringLength(100, ErrorMessage = "Technician name cannot exceed 100 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Surname is required.")]
+        [StringLength(100, ErrorMessage = "Surname cannot exceed 100 characters.")]
+        public string Surname{ get; set; }
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
