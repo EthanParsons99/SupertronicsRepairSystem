12268d8f8fa3a042e24e5df457363348851b0edb
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-10-26 10:26:21 +0200

    Product Management filter for serial number and price range

--- ViewModels/ProductListViewModel.cs
+++ ViewModels/ProductListViewModel.cs
@@ -1,9 +1,20 @@
 ﻿using SupertronicsRepairSystem.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace SupertronicsRepairSystem.ViewModels
 {
     public class ProductListViewModel
     {
+        [Display(Name = "Search Name/Serial")]
+        public string? SearchTerm { get; set; }
+
+        [Display(Name = "Min Price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Min Price cannot be negative.")]
+        public double? MinPrice { get; set; } = 0;
+
+        [Display(Name = "Max Price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Max Price cannot be negative.")]
+        public double? MaxPrice { get; set; }
         public List<Product> Products { get; set; } = new List<Product>();
 
     }
