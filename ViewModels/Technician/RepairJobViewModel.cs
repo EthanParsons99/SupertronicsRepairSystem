dd28ca290d122c40c5400bdfa0c334d8cafad271
PC <PC@CALWYN-PC>
2025-10-28 21:41:50 +0200

    displays serial number now and debugging removed

--- ViewModels/Technician/RepairJobViewModel.cs
+++ ViewModels/Technician/RepairJobViewModel.cs
@@ -5,6 +5,7 @@ namespace SupertronicsRepairSystem.ViewModels.Technician
         public string Id { get; set; }
         public string? ItemName { get; set; }
         public string? Status { get; set; }
+        public string? SerialNumber { get; set; }
         public string? CustomerName { get; set; }
         public DateTime LastUpdated { get; set; }
     }
