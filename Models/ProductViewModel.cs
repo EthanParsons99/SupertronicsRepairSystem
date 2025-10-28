0d586b49e76c5ef7de7f7e8df0ded863477086ac
Joshua Thomas <105593188+Joshcybr@users.noreply.github.com>
2025-10-29 00:13:39 +0200

    All keep asides  created and made dynamic

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Google.Cloud.Firestore;
+
+namespace SupertronicsRepairSystem.Models
+{
+    [FirestoreData]
+    public class ProductViewModel
+    {
+        [FirestoreProperty]
+        public string Name { get; set; }
+
+        [FirestoreProperty]
+        public string SerialNumber { get; set; }
+
+        [FirestoreProperty]
+        public string ImageUrl { get; set; }
+    }
+
+}
